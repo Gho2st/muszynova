20ecae4b790cfc1bf0920095193edb687d3c47ea
Dominik <dominik.jojczyk@gmail.com>
2025-03-17 23:11:11 +0100

    Update page.jsx

--- app/cennik/page.jsx
+++ app/cennik/page.jsx
@@ -1,12 +1,23 @@
 import Image from "next/image";
 
+export const metadata = {
+  title: "",
+  alternates: {
+    canonical: "https://muszynova.pl/cennik",
+  },
+
+  description: "",
+};
+
 export default function Cennik() {
   return (
     <>
-      <section className="py-20">
-        <h1 className="text-6xl font-bold text-customGold text-center mb-20">Cennik</h1>
-        <div className="px-6 xl:px-24 flex justify-center">
-          <div className="w-2/3">
+      <section className="py-20 bg-black xl:bg-white">
+        <h1 className="text-5xl xl:text-6xl font-bold text-customGold text-center mb-20">
+          Cennik
+        </h1>
+        <div className=" xl:px-24 flex justify-center">
+          <div className="xl:w-2/3">
             <Image
               src="/cennik/cennik.webp"
               alt=""
