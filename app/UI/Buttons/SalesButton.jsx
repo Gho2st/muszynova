c347aee318f3c549478ae695b56971f371c410bd
Dominik <dominik.jojczyk@gmail.com>
2025-03-29 19:23:46 +0100

    1.11

--- app/UI/Buttons/SalesButton.jsx
+++ app/UI/Buttons/SalesButton.jsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import { Link } from "@/i18n/routing";
 import { FaShoppingBasket } from "react-icons/fa";
 
 export default function SalesButton(props) {
--- app/UI/Nav/Nav.jsx
+++ app/UI/Nav/Nav.jsx
@@ -2,7 +2,7 @@
 import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import Image from "next/image";
-import Link from "next/link";
+import { Link } from "@/i18n/routing";
 import SalesButton from "../Buttons/SalesButton";
 import { IoIosArrowDown } from "react-icons/io";
 import SalesButton2 from "../Buttons/SalesButton2";
