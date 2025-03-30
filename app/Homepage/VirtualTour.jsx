068181a28848f0b9c15ce4ddcba1e561d2c1a68e
Dominik <dominik.jojczyk@gmail.com>
2025-03-30 14:39:46 +0200

    Update VirtualTour.jsx

--- app/Homepage/VirtualTour.jsx
+++ app/Homepage/VirtualTour.jsx
@@ -1,7 +1,7 @@
 const VirtualTour = ({ t }) => {
   return (
-    <section id="virtual-tour" className=" px-6 xl:px-44 py-20">
-      <h2 className="text-4xl xl:text-6xl font-extrabold  mb-24 text-center">
+    <section id="virtual-tour" className=" px-6 xl:px-44 py-16 xl:py-20">
+      <h2 className="text-4xl xl:text-6xl font-extrabold mb-16 xl:mb-24 text-center">
         {t("header")}
       </h2>
       <div className="h-[600px] xl:h-[750px]">
