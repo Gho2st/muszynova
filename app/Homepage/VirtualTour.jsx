614b04119dc2db622839daf563c66d8d64e4020e
Dominik <dominik.jojczyk@gmail.com>
2025-03-31 20:19:20 +0200

    Update VirtualTour.jsx

--- app/Homepage/VirtualTour.jsx
+++ app/Homepage/VirtualTour.jsx
@@ -4,7 +4,7 @@ const VirtualTour = ({ t }) => {
       <h2 className="text-4xl xl:text-6xl font-extrabold mb-16 xl:mb-24 text-center">
         {t("header")}
       </h2>
-      <div className="h-[600px] xl:h-[750px]">
+      <div className="h-[430px] lg:h-[600px] xl:h-[750px]">
         <iframe
           src="/virtual-tour/index.htm"
           className="h-[100%] w-1/1 mx-auto "
