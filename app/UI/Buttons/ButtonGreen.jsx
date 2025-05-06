8f4dff72193a5fa41e8d1002a59b23ceb0662322
Dominik <dominik.jojczyk@gmail.com>
2025-05-06 21:11:43 +0200

    1

--- app/UI/Buttons/ButtonGreen.jsx
+++ app/UI/Buttons/ButtonGreen.jsx
@@ -1,16 +1,14 @@
-import { Link } from "@/i18n/routing";
-
 export default function ButtonGreen({ text, link }) {
   return (
     <div>
       <button>
-        <Link
+        <a
           className={` text-white text-lg font-medium whitespace-nowrap flex justify-center items-center gap-2 p-4 clip-custom hover:clip-reverse bg-green-900 
           transition-all duration-300 transform hover:scale-105 hover:shadow-lg`} // Animacja i efekty na hover
           href={link}
         >
           {text}
-        </Link>
+        </a>
       </button>
     </div>
   );
