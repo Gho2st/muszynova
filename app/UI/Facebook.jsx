ce6db7a7c68a4096f3997fb3836e0a6081c05c70
Dominik <dominik.jojczyk@gmail.com>
2025-05-13 18:51:01 +0200

    Update Facebook.jsx

--- app/UI/Facebook.jsx
+++ app/UI/Facebook.jsx
@@ -6,6 +6,7 @@ export default function FacebookFeed() {
       <div className="flex flex-col md:flex-row justify-center items-stretch gap-6">
         <div className="w-full md:w-1/3 flex">
           <iframe
+            title="Facebook post 1 from Park Muszynova"
             src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FParkmuszynova%2Fposts%2Fpfbid02gySEU2RNVaB77UuXuW9cx1g99nfsgQneRK9AHZbxntYfCJ9JjHd3WbeHE94r5yTnl&show_text=true&width=500"
             className="w-full h-full min-h-[550px] lg:min-h-[600px]"
             style={{ border: "none", overflow: "hidden" }}
@@ -16,6 +17,7 @@ export default function FacebookFeed() {
 
         <div className="w-full md:w-1/3 flex">
           <iframe
+            title="Facebook post 2 from Park Muszynova"
             src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FParkmuszynova%2Fposts%2Fpfbid0XyxZ3WTgxZxJeRB1Gz1fTSbzU5tpR4QVvsMBBFP2r4a9nLsG5ec5WPWUcaBM3RJsl&show_text=true&width=500"
             className="w-full h-full min-h-[480px] lg:min-h-[600px]"
             style={{ border: "none", overflow: "hidden" }}
@@ -26,6 +28,7 @@ export default function FacebookFeed() {
 
         <div className="w-full md:w-1/3 flex">
           <iframe
+            title="Facebook post 3 from Park Muszynova"
             src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FParkmuszynova%2Fposts%2Fpfbid07aGVkKqCW8sdYu2VnAmtCNFjK9pEen55rP8KXrxbHJ9VTKZPKquPF9zPtyzmxV2Ll&show_text=true&width=500"
             className="w-full h-full min-h-[500px] lg:min-h-[600px]"
             style={{ border: "none", overflow: "hidden" }}
