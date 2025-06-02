8ba92508be500c0a80c5a676739d489f5ebd1477
Dominik <dominik.jojczyk@gmail.com>
2025-06-02 18:00:11 +0200

    1

--- app/Homepage/Hero.jsx
+++ app/Homepage/Hero.jsx
@@ -1,16 +1,25 @@
+import Image from "next/image";
 import FloatingImage from "./FloatingImage";
 
 export default function Hero({ t }) {
   return (
-    <div
-      className="px-3 pt-20 relative min-h-screen max-h-screen bg-cover bg-center"
-      style={{ backgroundImage: "url('/Muszynova-hotel.webp')" }}
-    >
+    <div className="relative min-h-screen max-h-screen">
+      {/* Użyj Image jako tła */}
+      <Image
+        src="/Muszynova-hotel.webp"
+        alt="Tło Muszynova"
+        layout="fill"
+        objectFit="cover"
+        quality={75}
+        priority // priorytet dla LCP
+        className="z-0"
+      />
+
       {/* Overlay */}
       <div className="absolute inset-0 bg-[#13322B]/30 z-10" />
 
-      {/* Treść i obraz w jednym kontenerze */}
-      <div className="relative z-20 flex flex-col items-center min-h-screen text-white gap-4">
+      {/* Treść i logo */}
+      <div className="relative z-20 flex flex-col items-center min-h-screen text-white gap-4 px-3 pt-20">
         <span className="mb-6 mt-4 md:mt-10 text-3xl md:text-5xl xl:w-1/2 xl:leading-snug font-extrabold text-center uppercase">
           {t("welcome")}
         </span>
