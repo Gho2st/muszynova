6e9137fa2e22530fd5224a5e00723fea5dc5e33d
Dominik <dominik.jojczyk@gmail.com>
2025-06-02 17:19:22 +0200

    Update Hero.jsx

--- app/Homepage/Hero.jsx
+++ app/Homepage/Hero.jsx
@@ -1,16 +1,25 @@
 import FloatingImage from "./FloatingImage";
+import Image from "next/image";
 
 export default function Hero({ t }) {
   return (
-    <div
-      className="px-3 pt-20 relative min-h-screen max-h-screen bg-cover bg-center"
-      style={{ backgroundImage: "url('/Muszynova-hotel.webp')" }}
-    >
+    <div className="relative min-h-screen max-h-screen overflow-hidden">
+      {/* Optymalizowany obraz tła z Next.js Image */}
+      <Image
+        src="/Muszynova-hotel.webp"
+        alt="Muszynova Hotel Background"
+        fill
+        priority
+        className="object-cover z-0"
+        quality={80} // zmień, jeśli obraz jest duży
+        sizes="100vw"
+      />
+
       {/* Overlay */}
       <div className="absolute inset-0 bg-[#13322B]/30 z-10" />
 
-      {/* Treść i obraz w jednym kontenerze */}
-      <div className="relative z-20 flex flex-col items-center min-h-screen text-white gap-4">
+      {/* Treść */}
+      <div className="relative z-20 flex flex-col items-center min-h-screen text-white gap-4 px-3 pt-20">
         <span className="mb-6 mt-4 md:mt-10 text-3xl md:text-5xl xl:w-1/2 xl:leading-snug font-extrabold text-center uppercase">
           {t("welcome")}
         </span>
