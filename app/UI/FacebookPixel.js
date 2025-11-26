4da1215e1019d99f23833cfc4d9cf4416a3579f5
Dominik <dominik.jojczyk@gmail.com>
2025-11-26 09:34:38 +0100

    Update FacebookPixel.js

--- app/UI/FacebookPixel.js
+++ app/UI/FacebookPixel.js
@@ -28,7 +28,7 @@ const PixelTracker = () => {
             }(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
 
-            fbq('init', process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID);
+            fbq('init', ${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID});
             fbq('track', 'PageView');
           `,
         }}
